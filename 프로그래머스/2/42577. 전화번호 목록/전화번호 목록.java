efbba3f10ee48065bd7848751ef21c55e97ed665
Eunseo Yu <73543187+221B0825@users.noreply.github.com>
2024-03-20 21:48:40 +0900

    [level 2] Title: 전화번호 목록, Time: 380.66 ms, Memory: 202 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -3,21 +3,23 @@ import java.util.*;
 class Solution {
     public boolean solution(String[] phone_book) {
         boolean answer = true;
-        HashMap<String, Integer> map = new HashMap<>();
-        
         int index = 0;
+        
+        HashMap<String, Integer> numberList = new HashMap<>();
+        
         for(String number : phone_book){
-            map.put(number, index++);
+            numberList.put(number, index++);
         }
         
-        for(int i=0; i<phone_book.length;i++){
-            for(int j=0; j<phone_book[i].length(); j++){
-                if(map.containsKey(phone_book[i].substring(0, j))){
+        for(Map.Entry<String, Integer> number : numberList.entrySet()){
+            for(int i=0; i<number.getKey().length(); i++){
+                if(numberList.containsKey(number.getKey().substring(0, i))){
                     return false;
                 }
             }
         }
-    return answer;
+        
+        
+        return answer;
     }
-    
 }
