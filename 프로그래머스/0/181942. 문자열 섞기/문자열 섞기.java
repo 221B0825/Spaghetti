32926e846653a6b2efbb5eee12f8a70f045e3bab
Eunseo Yu <73543187+221B0825@users.noreply.github.com>
2024-04-20 00:45:15 +0900

    Update 문자열 섞기.java

--- 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
@@ -2,9 +2,8 @@ class Solution {
     public String solution(String str1, String str2) {
         char[] char1 = str1.toCharArray();
         char[] char2 = str2.toCharArray();
-        char[] result = new char[char1.length + char2.length];
         String answer = "";
-        for(int i=0; i<result.length; i++){
+        for(int i=0; i<char1.length + char2.length; i++){
             if(i % 2 == 0){
                 answer += char1[i/2];
             }else{
